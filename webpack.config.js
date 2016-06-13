5450cb9f31fbc337b4ad48768b7a7b4741b64c49
Jake Sendar <jakesendar@gmail.com>
2016-06-12 23:46:09 -0400

    add core files

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+     'react-hot-loader/patch',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'src')
+    }]
+  },
+  resolve: {
+    root: [
+      path.resolve('./src')
+    ],
+    modulesDirectories: [
+      'lib', 'node_modules'
+    ]
+  }
+};
